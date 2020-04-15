996c83cf13d94da474c8e5fde5654af9786c5eaf
Yoel M Galarza <yoel.meiker@gmail.com>
2020-04-15 18:13:04 -0300

    Finishing the to-do list !

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,25 +1,36 @@
 //check off specific todos by clicking
-$("ul").on('click', "li", function(){
+$("ul").on('click', "li", function () {
     $(this).toggleClass("done");
 
 });
 // click on x to delete to do
-$("ul").on("click", "span", function(ev){
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove() ; 
+$("ul").on("click", "span", function (ev) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
     });
-    event.stopPropagation();
+    ev.stopPropagation();
 });
 // take the input value and make a new to do
-$("input[type='text']").keypress(function(event){
-    if(event.which === 13){
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
         //grab new to do text from input
-        var todoText= $(this).val();
-        if(todoText !== ""){
-        $(this).val("");
-        //create a new li and add to ul
-        $("ul").append("<li><span>x</span> " + todoText + "</li>");
+        var todoText = $(this).val();
+        if (todoText !== "") {
+            $(this).val("");
+            //create a new li and add to ul
+            $("ul").append('<li><span><i class="fas fa-trash-alt"></i></span>' + todoText + "</li>");
         }
         else return
     }
-    });
+});
+$(".fa-plus").on('click',function(){
+    $('input').fadeToggle();
+});
+$('input').mouseenter(function(){
+   $('input').css('cursor', 'text');
+   $('input').attr('placeholder', "")
+});
+$('input').mouseout(function(){
+   $('input').css('cursor', 'auto');
+   $('input').attr('placeholder', "Add New To-Do")
+});
